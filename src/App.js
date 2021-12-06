218def4cde069d12e5650342fbdd505f95b9424f
Mark <mark3fullen@gmail.com>
2021-12-06 00:43:18 -0500

    Big Patch 2.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import './App.css';
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import Home from './lib/Home'
-import NavBar from './lib/NavBar'
-import Resume from './lib/Resume';
-import Contact from './lib/Contact'
+import Home from './Home'
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter className="App">
-      <NavBar className="NavBar"/>
-        <Route exact path="/" >
-          <Home className="Home"/>
-        </Route>
-        <Route exact path="/resume">
-          <Resume />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-      </BrowserRouter>
+    <div className="app">
+      <Home className="home"/>
     </div>
   );
 }
